41a581f9b574b8526f97436aeee963b802f228b3
Kame <ev60537@meiji.ac.jp>
2019-02-21 21:12:41 +0900

    add animejs

--- src/codeStage.js
+++ src/codeStage.js
@@ -1,3 +1,4 @@
+// import anime from '../node_modules/animejs/lib/anime.es.js';
 
 const hljsKeywords = [
     "hljs-keyword",
@@ -14,65 +15,81 @@ const hljsKeywords = [
     "hljs-title",
 ]
 
-let sentenceParams;
+let sentenceParamsTop = [];
+let sentenceParamsBottom = [];
+
+let createParamsJson = (file, codeContainer, sentenceParams, isTop) => {
+    if (file===null) return;
+    let fileReader = new FileReader();
+    fileReader.onload = () => {
+        const code = fileReader.result;
+        codeContainer.innerHTML = hljs.highlightAuto(code).value;
+
+        const sentenceArray = codeContainer.innerText.split(/\r\n|\r|\n/);
+        const lengthArray = sentenceArray.map((x)=>x.length);
+
+        htmlArray = codeContainer.innerHTML.split(/\r\n|\r|\n/);
+        const keywordArray = htmlArray.map((row)=>{
+            resArray = [];
+            hljsKeywords.map((key)=>{
+                if (row.match(key)) {
+                    resArray.push(key);
+                }
+            });
+            return resArray;
+        })
+        console.log(htmlArray);
+
+        // JSONの作成
+        // sentenceParams = [];
+        let len = sentenceArray.length;
+        for(let i=0; i<len; i++){
+            sentenceParams.push({
+                length: lengthArray[i],
+                hljsKeywords: keywordArray[i],
+            });
+        }
+
+        stagePart = isTop ? "top" : "bottom";
+        document.getElementById(`code-stage-${stagePart}`).className += ` ${stagePart}-active`
+        resizeCanvas(codeContainer.offsetHeight, 800);
+    }
+    fileReader.readAsText(file);
+}
 
 function setup() {
     let canvas = createCanvas(1000, 800);
     canvas.parent('#sketch-container');
 
-    let files;
-    let fileSelector = select('#file-selector-top');
-    fileSelector.elt.onchange = (ev) => {
-        files = ev.currentTarget.files;
-    };
-
-    let highlightButton = select('#highlight-button-top');
-    highlightButton.elt.onclick = () => {
-        if (files[0]===null) return;
-        const file = files[0];
-        let fileReader = new FileReader();
-        fileReader.onload = () => {
-            const code = fileReader.result;
-            let codeContainer = document.getElementById("code-container");
-            codeContainer.innerHTML = hljs.highlightAuto(code).value;
-
-            const sentenceArray = codeContainer.innerText.split(/\r\n|\r|\n/);
-            const lengthArray = sentenceArray.map((x)=>x.length);
-
-            htmlArray = codeContainer.innerHTML.split(/\r\n|\r|\n/);
-            const keywordArray = htmlArray.map((row)=>{
-                resArray = [];
-                hljsKeywords.map((key)=>{
-                    if (row.match(key)) {
-                        resArray.push(key);
-                    }
-                });
-                return resArray;
-            })
-            console.log(htmlArray);
-
-            // JSONの作成
-            sentenceParams = [];
-            let len = sentenceArray.length;
-            for(let i=0; i<len; i++){
-                sentenceParams.push({
-                    length: lengthArray[i],
-                    hljsKeywords: keywordArray[i],
-                });
-            }
-
-            document.getElementById("code-stage").className += " bottom-active";
-            resizeCanvas(codeContainer.offsetHeight, 800);
+    let topFiles;
+    select('#file-selector-top').elt.onchange = (ev) => topFiles = ev.currentTarget.files;
+    let bottomFiles;
+    select('#file-selector-bottom').elt.onchange = (ev) => bottomFiles = ev.currentTarget.files;
+    
+    select('#highlight-button').elt.onclick = () => {
+        if (topFiles) {
+            const codeContainerTop = document.getElementById("code-container-top");
+            createParamsJson(topFiles[0], codeContainerTop, sentenceParamsTop, true);
         }
-        fileReader.readAsText(file);
+        if (bottomFiles) {
+            const codeContainerBottom = document.getElementById("code-container-bottom");
+            createParamsJson(bottomFiles[0], codeContainerBottom, sentenceParamsBottom, false);
+        }
     };
 }
 
-function draw() {    
+function draw() {
     background(200);
 
-    if (sentenceParams) {
-        sentenceParams.forEach((v, i) => {
+    if (sentenceParamsTop) {
+        sentenceParamsTop.forEach((v, i) => {
+            fill(0, 255, 0);
+            rect(i*30, 1, 30, 10*v.length);
+        });
+    }
+
+    if (sentenceParamsBottom) {
+        sentenceParamsBottom.forEach((v, i) => {
             fill(255, 0, 0);
             rect(i*30, height-1, 30, -10*v.length);
         });
